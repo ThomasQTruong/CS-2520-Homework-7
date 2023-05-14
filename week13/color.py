3226cc745438a626c87174baef2cac8bf5bb3ac1
Thomas Truong <TruongThomasQ@gmail.com>
2023-05-14 03:58:43 -0700

    Added more colors.

--- week13/color.py
+++ week13/color.py
@@ -13,6 +13,8 @@ class Color:
   WHITE = (255, 255, 255)
   BLACK = (0, 0, 0)
   RED = (255, 0, 0)
+  GREEN = (0, 255, 0)
+  BLUE = (0, 0, 255)
 
   @staticmethod
   def rand_color() -> tuple[int, int, int]:
